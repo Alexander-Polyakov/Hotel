7540cf9b3d522c3c44dfc4c37fde67ab0142cf47
Alexander <aleksandr_1992_15@mail.ru>
2017-11-06 16:00:41 +0300

    initial

---
+++ src/js/main.js
@@ -0,0 +1,27 @@
+ymaps.ready(function () {
+    var center = [51.65099076275837,39.28873186441802];
+    var myMap = new ymaps.Map('map', {
+        center: center,
+        zoom: 15,
+        controls: ['zoomControl']
+    });
+    myMap.behaviors.disable('scrollZoom');
+
+    myPlacemark = new ymaps.Placemark(myMap.getCenter(), {
+
+    }, {
+        iconLayout: 'default#image',
+        iconImageHref: '../images/svg/icon-metka.svg',
+        iconImageSize: [35, 50]
+    });
+
+
+    myMap.geoObjects.add(myPlacemark);
+
+});
+
+$(document).ready(function() {
+    $('.js-inputmask-phone').mask("+7 (999) 999-99-99");
+
+
+});
