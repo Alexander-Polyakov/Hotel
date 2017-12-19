fdcd668ce121c36a5fd0899ecce20ff8824040d2
Alexander <aleksandr_1992_15@mail.ru>
2017-12-19 15:12:32 +0300

    finish 2.0

--- build/js/map.js
+++ build/js/map.js
@@ -1,12 +1,12 @@
 function initMap() {
-    var center = {lat: 46.4027, lng: 8.13693};
+    var center = {lat: 46.4035, lng: 8.133};
     var map = new google.maps.Map(document.getElementById('map'), {
         zoom: 17,
         center: center
     });
 
     var marker = new google.maps.Marker({
-        position: center,
+        position: {lat: 46.4027, lng: 8.13693},
         map: map
     });
 }
